ac57a57f2867bc098f488b4452ecf8fe344b8540
Maasuuum <fardousalmasum09@gmail.com>
2024-04-30 16:57:05 +0600

    janina

--- js/deposit.js
+++ js/deposit.js
@@ -1,7 +1,15 @@
+function getInpuFieldtValueById(inputId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    return inputFieldValue;
+}
+
 document.getElementById('btn-deposit').addEventListener('click', function(){
     /*
     1. get the element by id
     2. get the value from the element
     3. convert string value to a number
     */
+   const newDepositAmount = getInpuFieldtValueById('deposit-field')
 })
