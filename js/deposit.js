502b683c03e73aa320da48e5af67b917e8cacdfa
Maasuuum <fardousalmasum09@gmail.com>
2024-04-30 15:05:01 +0600

    utilities withdraws and deposit js file created

--- js/deposit.js
+++ js/deposit.js
@@ -1,1 +1,7 @@
-console.log('deposit button created')
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    /*
+    1. get the element by id
+    2. get the value from the element
+    3. convert string value to a number
+    */
+})
