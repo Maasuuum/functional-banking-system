1ed57736c1251741414c0e01bc0664b98598283e
Maasuuum <fardousalmasum09@gmail.com>
2024-05-01 09:25:33 +0600

    ghorar dim kicchu bujhi ni

--- js/deposit.js
+++ js/deposit.js
@@ -1,8 +1,16 @@
-function getInpuFieldtValueById(inputId){
+function getInputFieldValueById(inputFieldId){
     const inputField = document.getElementById(inputFieldId);
     const inputFieldValueString = inputField.value;
     const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
     return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById('elementId');
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
 }
 
 document.getElementById('btn-deposit').addEventListener('click', function(){
@@ -11,5 +19,6 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     2. get the value from the element
     3. convert string value to a number
     */
-   const newDepositAmount = getInpuFieldtValueById('deposit-field')
+
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 })
