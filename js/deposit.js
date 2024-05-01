501d9dec85c53d388a38603c9a20efb81a6fa5b1
Maasuuum <fardousalmasum09@gmail.com>
2024-05-01 12:13:02 +0600

    jhankar bhai bolce next video te bujhe jabo

--- js/deposit.js
+++ js/deposit.js
@@ -1,18 +1,3 @@
-function getInputFieldValueById(inputFieldId){
-    const inputField = document.getElementById(inputFieldId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldValue = parseFloat(inputFieldValueString);
-    inputField.value = '';
-    return inputFieldValue;
-}
-
-function getElementValueById(elementId){
-    const element = document.getElementById('elementId');
-    const elementValueString = element.innerText;
-    const value = parseFloat(elementValueString);
-    return value;
-}
-
 document.getElementById('btn-deposit').addEventListener('click', function(){
     /*
     1. get the element by id
@@ -21,4 +6,21 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     */
 
     const newDepositAmount = getInputFieldValueById('deposit-field');
+    /*
+    1. get previous deposit total by id
+    */
+   const previousDepositTotal = getTextElementValueById('deposit-total');
+
+//    calculate new deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+
+    // set deposit total value
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    // get previous balance by using the function
+    const previousBalanceTotal = getTextElementValueById('balance-total')
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+
+
 })
